dd3e44f4e40e92a443c8a1fda279da3038e7093d
mahadi-hasan-rumi <mahadi528@gmail.com>
2020-08-08 03:40:07 +0600

    done little dues need to be recovered

---
+++ letConst.js
@@ -0,0 +1,21 @@
+const hubby = 'reah'
+// hubby = "nawar"
+console.log(hubby)
+const numbers = [12, 45];
+numbers[1] = 42;
+numbers.push(12, 25);
+// numbers = [12, 45, "rni", "jhini", " mini"]
+const person = {name:"rakib", phone: 4582}
+console.log(person)
+console.log(numbers);
+
+let patientName = "rahim chacha"
+patientName = "fatema khala"
+console.log(patientName);
+
+sum = 0;
+for(let i = 0; i<10 ; i++ ){
+    sum = sum + i;
+
+console.log(sum, i)
+}
---
+++ template.js
@@ -0,0 +1,16 @@
+const firstName ="mahadi" 
+const lastName ="hasan"
+const fullName = firstName +" " + lastName + " u are lazy boy";
+console.log(fullName);
+const  fullName2 = `${firstName} ${lastName} i miss you.`
+console.log(fullName2);
+
+const multiLine = "look at the board\n"
++"read the letter\n"
++" and write down your home work "
+console.log(multiLine)
+ 
+const multiLine2 = `dont loose hope
+keep your mind stronger
+dont look back`
+console.log(multiLine2);
